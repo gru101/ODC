2bb8235840938fff5f0a0a91f5a2a7388faa47a9
gru101 <grukandgal@gmail.com>
2024-12-31 19:08:32 +0530

    fixed_allowed_hosts

--- one_day_contract/settings.py
+++ one_day_contract/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['odc-red.vercel.app/']
+ALLOWED_HOSTS = ['odc-red.vercel.app']
 
 
 # Application definition
